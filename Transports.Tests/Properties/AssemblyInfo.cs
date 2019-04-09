e61f514f2ce1fb4f9955507fab34f120cb39dce0
bogdaner2 <KonorinDeveloper@gmail.com>
2019-04-09 21:26:21 +0300

    Fix

--- Transports.Tests/Properties/AssemblyInfo.cs
+++ Transports.Tests/Properties/AssemblyInfo.cs
@@ -1,7 +1,10 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("Transports.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -11,10 +14,23 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: Guid("0eeac3d6-3472-455d-9e45-bd649c5015e9")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("da2baf95-5b68-4f0c-b60e-fd159ddd7715")]
 
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- Transports.Tests/ServiceTests.cs
+++ Transports.Tests/ServiceTests.cs
@@ -7,24 +7,29 @@ using Transports.Core.DependencyInjection;
 using Transports.Core.Interfaces;
 using Transports.Core.Models;
 using Transports.Core.Models.SQL;
+using Transports.Tests.Proxy;
 
 namespace Transports.Tests
 {
     [TestFixture]
     public class ServiceTests
     {
+        private int addedItems = 0;
+        private int removedItems = 0;
+        private TransportsServiceClient _wcfClient;
+
+
+        List<Driver> drivers = new List<Driver>
+        {
+            new Driver { DriverId = Guid.NewGuid(), Name = "test", Age = 18, Rang = RangEnum.A},
+            new Driver { DriverId = Guid.NewGuid(), Name = "test1", Age = 18, Rang = RangEnum.A},
+            new Driver { DriverId = Guid.NewGuid(), Name = "test2", Age = 18, Rang = RangEnum.A}
+        };
+
         [SetUp]
         public void Init()
         {
-            var addedItems = 0;
-            var removedItems = 0;
-
-            var drivers = new List<Driver>
-            {
-                new Driver { DriverId = Guid.NewGuid(), Name = "test", Age = 18, Rang = RangEnum.A},
-                new Driver { DriverId = Guid.NewGuid(), Name = "test1", Age = 18, Rang = RangEnum.A},
-                new Driver { DriverId = Guid.NewGuid(), Name = "test2", Age = 18, Rang = RangEnum.A}
-            };
+            _wcfClient = new TransportsServiceClient();
             var mock = new Mock<IRepository<Driver>>();
             mock.Setup(r => r.GetAll()).Returns(drivers);
             mock.Setup(r => r.Get(It.IsAny<Expression<Func<Driver, bool>>>())).Returns(drivers[new Random().Next(0, 2)]);
@@ -33,7 +38,7 @@ namespace Transports.Tests
                 addedItems++;
                 drivers.Add(item);
             });
-            mock.Setup(r => r.Remove(It.IsAny<Driver>())).Callback((bool result) => removedItems++);
+            mock.Setup(r => r.Remove(It.IsAny<Driver>())).Callback((Driver result) => removedItems++);
 
             ServiceLocator.SetDriverRepo(mock.Object);
         }
@@ -41,7 +46,6 @@ namespace Transports.Tests
         [Test]
         public void Return_Driver_Collection()
         {
-            ServiceLocator.SetDriverRepo(null);
         }
     }
 }
