502f17ae76dabed2ac6ab616c34613c8707ec857
bogdaner2 <KonorinDeveloper@gmail.com>
2020-02-24 22:03:36 +0200

    Added UI tests

--- Transports.UI_Tests/UITest.cs
+++ Transports.UI_Tests/UITest.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
@@ -12,12 +16,123 @@ namespace Transports.UI_Tests
         public void Init()
         {
             driver = new ChromeDriver();
+            driver.Url = "http://localhost:3000/";
         }
 
         [Test]
-        public void FirstTest()
+        public void TestNewDriver()
         {
-            driver.Url = "http://localhost:3000/";
+            Thread.Sleep(2000);
+            var initCount = driver.FindElements(By.ClassName("item")).Count;
+
+            CreateNewDriver();
+
+            var resCount = driver.FindElements(By.ClassName("item")).Count;
+
+            Assert.AreNotEqual(initCount, resCount);
+        }
+
+        [Test]
+        public void TestUpdate()
+        {
+            Thread.Sleep(2000);
+            var items = driver.FindElements(By.ClassName("item"));
+            var item = items.Last();
+            var controls = item.FindElement(By.ClassName("controls"));
+            var updateBtn = controls.FindElement(By.ClassName("custom-button-primary"));
+            updateBtn.Click();
+
+            driver.FindElement(By.Id("name")).SendKeys("Update");
+
+            driver.FindElement(By.Id("submit")).Click();
+
+            Thread.Sleep(1000);
+
+            var value = item.FindElements(By.TagName("div"))
+                .Skip(1).Take(1).First()
+                .FindElements(By.TagName("span")).Skip(1).Take(1).First().Text;
+
+            Assert.IsTrue(value.Contains("Update"));
+        }
+
+        [Test]
+        public void TestClear()
+        {
+            Thread.Sleep(2000);
+            var items = driver.FindElements(By.ClassName("item"));
+            var item = items.Last();
+            var controls = item.FindElement(By.ClassName("controls"));
+            var updateBtn = controls.FindElement(By.ClassName("custom-button-primary"));
+            updateBtn.Click();
+            Thread.Sleep(2000);
+            var clearButton = driver.FindElement(By.Id("clear"));
+
+            Assert.AreEqual(clearButton.Displayed, true);
+
+            var name = driver.FindElement(By.Id("name")).GetAttribute("value");
+
+            clearButton.Click();
+
+            Thread.Sleep(500);
+
+            var clearedName = driver.FindElement(By.Id("name")).GetAttribute("value"); 
+            
+            Assert.AreNotEqual(name, clearedName);
+
+            try
+            {
+                var clearBtn = driver.FindElement(By.Id("clear"));
+            }
+            catch (Exception e)
+            {
+                Assert.IsFalse(false);
+            }
+        }
+
+        [Test]
+        public void TestDelete()
+        {
+            Thread.Sleep(2000);
+            var items = driver.FindElements(By.ClassName("item"));
+            var item = items.Last();
+            var controls = item.FindElement(By.ClassName("controls"));
+            var deleteBtn = controls.FindElement(By.ClassName("custom-button-accent"));
+            deleteBtn.Click();
+            Thread.Sleep(1000);
+
+            Assert.AreNotEqual(items.Count, driver.FindElements(By.ClassName("item")));
+        }
+
+        [Test]
+        public void CheckDriversList()
+        {
+            var initCount = driver.FindElements(By.ClassName("item")).Count;
+
+            Thread.Sleep(2000);
+
+            var resCount = driver.FindElements(By.ClassName("item")).Count;
+
+            Assert.AreNotEqual(initCount, resCount);
+        }
+
+        [Test]
+        public void ToggleFrame()
+        {
+            driver.FindElement(By.Id("togle_frame")).Click();
+            var frame = driver.FindElement(By.TagName("iframe"));
+
+            Thread.Sleep(2000);
+
+            Assert.AreEqual(true, frame.Displayed);
+        }
+
+        private void CreateNewDriver()
+        {
+            driver.FindElement(By.Id("name")).SendKeys("Updated");
+            driver.FindElement(By.Id("age")).SendKeys("20");
+            driver.FindElement(By.Id("rang")).SendKeys("1");
+            driver.FindElement(By.Id("submit")).Click();
+            Thread.Sleep(2000);
         }
 
         [TearDown]
--- transports-frontend/src/App.js
+++ transports-frontend/src/App.js
@@ -7,15 +7,96 @@ const api = 'http://localhost:59257';
 function App() {
   const [drivers, setDrivers] = useState([]);
   const [showFrame, setShowFrame] = useState(false);
+
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(16);
+  const [rang, setRang] = useState(1);
+
+  const [isUpdate, setIsUpdate] = useState(false);
+  const [updateItemId, setUpdateItemId] = useState();
+
   const loadDrivers = () => {
     axios.get(api + '/api/drivers').then(({data}) => {
       setDrivers(data);
     })
   }
 
+  const deleteDriver = (id) => {
+    axios.delete(api + '/api/drivers?id=' + id).then(() => {
+      setDrivers(drivers.filter(x => x.DriverId !== id));
+    })
+  }
+
+  const createDriver = () => {
+    setName('');
+    setAge(16);
+    setRang(1);
+    axios.post(api + '/api/drivers', {
+      Name: name,
+      Age: age,
+      Rang: rang
+    }).then(loadDrivers);
+  }
+
+  const updateDriver = () => {
+    setName('');
+    setAge(16);
+    setRang(1);
+    axios.put(api + '/api/drivers', {
+      DriverId: updateItemId,
+      Name: name,
+      Age: age,
+      Rang: rang
+    }).then(loadDrivers);
+  }
+
+  const submit = () => {
+    if(isUpdate) {
+      updateDriver();
+      setIsUpdate(false);
+      setUpdateItemId('');
+      return;
+    }
+    createDriver();
+  }
+
+  const clear = () => {
+    setIsUpdate(false);
+    setUpdateItemId('');
+    setName('');
+    setAge(16);
+    setRang(1);
+  }
+
+  const setUpdate = (id) => {
+    setIsUpdate(true);
+    setUpdateItemId(id);
+    const driver = drivers.find(x => x.DriverId === id);
+    setName(driver.Name);
+    setAge(driver.Age);
+    setRang(driver.Rang);
+  }
+
+  const handleFrameMessage = ({data}) => {
+    let recievedData;
+    try {
+      recievedData = JSON.parse(data);
+    }
+    catch(e) { return;}
+
+    if(recievedData.type === 'DELETE') {
+      setDrivers(drivers.filter(x => x.DriverId !== recievedData.payload));
+    }
+
+    if(recievedData.type === 'POST' || recievedData.type === 'PUT') {
+      loadDrivers()
+    }
+  }
+
   useEffect(() => {
     loadDrivers();
-  })
+  }, []
+  )
 
   return (<Fragment>
     <div className='container'>
@@ -33,26 +114,61 @@ function App() {
               {
                 !showFrame &&
                 <div className="controls">
-                  <button className='custom-button custom-button-primary'>Update</button>
-                  <button className='custom-button custom-button-accent'>Delete</button>
+                  <button className='custom-button custom-button-primary' onClick={() => setUpdate(item.DriverId)}>Update</button>
+                  <button onClick={() => deleteDriver(item.DriverId)} className='custom-button custom-button-accent'>Delete</button>
                 </div>
               }
             </div>
           })
         }
-        </div>
-        <div>
-          FORM
-        </div>
+        </div> 
+        {
+          !showFrame && (
+            <div className='form-container'>
+            <form>
+              <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input id="name" type="text" aria-describedby="emailHelp" value={name} onChange={(event) => setName(event.target.value)} placeholder="Enter name" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="age">Age</label>
+                <input type="number" id="age" value={age} onChange={(event) => setAge(event.target.value)} placeholder="Age" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="rang">Rang</label>
+                <input type="number" id="rang" placeholder="Rang" onChange={(event) => setRang(event.target.value)} value={rang} />
+              </div>
+              <div className='form-group'>
+                <input type="button" id='submit' value='Submit' onClick={() => submit()}/>
+              </div>
+              {
+                isUpdate && 
+                  <div className='form-group'>
+                  <input type="button" id='clear' style={{ background: '#606040'}} value='Clear' onClick={() => clear()}/>
+                </div>
+              }
+            </form>
+          </div>
+          )
+        }
     </div>
-    <button className='togleframe' onClick={() => setShowFrame(!showFrame)}>Use IFrame</button>
+    <button id='togle_frame' onClick={() => {
+      if(showFrame) {
+        setShowFrame(false);
+        window.addEventListener('message', handleFrameMessage, false);
+        return;
+      }
+
+      setShowFrame(true);
+      window.addEventListener('message', handleFrameMessage, false);
+
+    } }>Use IFrame</button>
     {
       showFrame &&
         <div className='frame-container'>
           <iframe title='frame' src={api} width='1200' height='400' />
         </div>
     }
-
     </Fragment>);
 }
 
